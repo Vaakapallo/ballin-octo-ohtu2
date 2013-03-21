cd1752dbcc444c92dbe7f29bfb4b5637b1b95734
Vaakapallo <Lassi.Vapaakallio@cs.helsinki.fi>
2013-03-21 16:05:47 +0200

    tehtävät 1-3

---
+++ viikko2/Ohtu-NhlStatistics1/src/test/java/testit/StatisticsTest.java
@@ -0,0 +1,63 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testit;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
+import ohtuesimerkki.Player;
+import ohtuesimerkki.Statistics;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author lvapaaka
+ */
+public class StatisticsTest {
+
+    Statistics stats;
+    Reader readerStub = new Reader() {
+        public List<Player> getPlayers() {
+            ArrayList<Player> players = new ArrayList<Player>();
+
+            players.add(new Player("Semenko", "EDM", 4, 12));
+            players.add(new Player("Lemieux", "PIT", 45, 54));
+            players.add(new Player("Kurri", "EDM", 37, 53));
+            players.add(new Player("Yzerman", "DET", 42, 56));
+            players.add(new Player("Gretzky", "EDM", 35, 89));
+
+            return players;
+        }
+    };
+    
+    
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
