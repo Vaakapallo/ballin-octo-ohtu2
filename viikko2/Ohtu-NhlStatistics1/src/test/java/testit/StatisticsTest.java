c9aa5c8459412c8c720dcf2e41b833b45a73b1af
Vaakapallo <lassi.vapaakallio@cs.helsinki.fi>
2013-03-24 13:43:27 +0200

    StatisticsTest

--- viikko2/Ohtu-NhlStatistics1/src/test/java/testit/StatisticsTest.java
+++ viikko2/Ohtu-NhlStatistics1/src/test/java/testit/StatisticsTest.java
@@ -5,11 +5,11 @@
 package testit;
 
 import java.io.IOException;
-import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
 import ohtuesimerkki.Player;
 import ohtuesimerkki.Statistics;
+import ohtuesimerkki.Reader;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -37,27 +37,53 @@ public class StatisticsTest {
             return players;
         }
     };
-    
-    
 
-    @BeforeClass
-    public static void setUpClass() {
-    }
-
-    @AfterClass
-    public static void tearDownClass() {
-    }
-
-    @Before
-    public void setUp() {
-    }
-
-    @After
-    public void tearDown() {
-    }
     // TODO add test methods here.
     // The methods must be annotated with annotation @Test. For example:
     //
     // @Test
     // public void hello() {}
+    public StatisticsTest() {
+        this.stats = new Statistics(readerStub);
+    }
+
+    @Test
+    public void objectIsNotNull() {
+        assertNotNull(stats);
+    }
+
+    @Test
+    public void searchFindsTheRightPlayer() {
+        assertEquals(stats.search("Semenko").toString(), new Player("Semenko", "EDM", 4, 12).toString());
+    }
+
+    @Test
+    public void searchReturnsNullIfPLayerNotFound() {
+        assertNull(stats.search("nlkjsdfnkjsdfjnk"));
+    }
+
+    @Test
+    public void teamReturnsTheRightAmountOfPLayers() {
+        assertEquals(stats.team("EDM").size(), 3);
+    }
+
+    @Test
+    public void teamReturnsAnEmptyListForInvalidTeam() {
+        assertTrue(stats.team("TPR").isEmpty());
+    }
+
+    @Test
+    public void teamFindsTheRightFirstPlayerOfATeam() {
+        assertEquals(stats.team("DET").get(0).toString(), new Player("Yzerman", "DET", 42, 56).toString());
+    }
+
+    @Test
+    public void topScorerFindstheBestPlayer() {
+        assertEquals(stats.topScorers(1).get(0).toString(), new Player("Gretzky", "EDM", 35, 89).toString());
+    }
+
+    @Test
+    public void topScorersReturnsAnEmptyListWithNegative() {
+        assertTrue(stats.topScorers(-5).isEmpty());
+    }
 }
