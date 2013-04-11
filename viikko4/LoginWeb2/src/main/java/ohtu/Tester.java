4f7cdfc82f2afcb2a7bfeb07da46ff75f6a7aa50
Vaakapallo <Lassi.Vapaakallio@cs.helsinki.fi>
2013-04-11 14:12:52 +0300

    Teht 2 97% valmis

--- viikko4/LoginWeb2/src/main/java/ohtu/Tester.java
+++ viikko4/LoginWeb2/src/main/java/ohtu/Tester.java
@@ -59,7 +59,75 @@ public class Tester {
 
         System.out.println("==");
         System.out.println(driver.getPageSource());
+        element = driver.findElement(By.linkText("back to home"));
+        element.click();
 
         System.out.println("==");
+
+        System.out.println("UUDEN KÄYTTÄJÄN LUOMINEN ALKAA");
+
+        System.out.println(driver.getPageSource());
+        element = driver.findElement(By.linkText("register new user"));
+        element.click();
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("pekkaparka");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("parkap3kka");
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("parkap3kka");
+        element = driver.findElement(By.name("add"));
+        element.submit();
+
+        System.out.println("==");
+        System.out.println(driver.getPageSource());
+        element = driver.findElement(By.linkText("continue to application mainpage"));
+        element.click();
+        element = driver.findElement(By.linkText("logout"));
+        element.click();
+
+        System.out.println("==");
+
+        System.out.println("UUDEN KÄYTTÄJÄN LUOMINEN JA KIRJAUTUMINEN ALKAA");
+
+        System.out.println(driver.getPageSource());
+        element = driver.findElement(By.linkText("register new user"));
+        element.click();
+        System.out.println("==");
+        System.out.println(driver.getPageSource());
+
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("partapekka");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("parkap3kka");
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("parkap3kka");
+        element = driver.findElement(By.name("add"));
+        element.submit();
+
+        System.out.println("==");
+        System.out.println(driver.getPageSource());
+        element = driver.findElement(By.linkText("continue to application mainpage"));
+        element.click();
+        element = driver.findElement(By.linkText("logout"));
+        element.click();
+
+        element = driver.findElement(By.linkText("login"));
+        element.click();
+
+        System.out.println("==");
+
+        System.out.println(driver.getPageSource());
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("pekka");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("akkep");
+        element = driver.findElement(By.name("login"));
+        element.submit();
+
+        System.out.println("==");
+        System.out.println(driver.getPageSource());
+
+        System.out.println("==");
+
     }
 }
